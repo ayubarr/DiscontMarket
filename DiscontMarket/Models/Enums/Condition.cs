549fb92c30dea1acdcd334a38619572e65679b6b
ayub <ayub.arbiev@gmail.com>
2024-11-09 17:31:34 +0300

    add Condition enum for Order entity

---
+++ DiscontMarket/Models/Enums/Condition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscontMarket.Domain.Models.Enums
+{
+    public enum Condition
+    {
+        Pending = 1,
+        Confirmed = 2,
+        Canceled = 3,
+        Failed = 4,
+    }
+}
