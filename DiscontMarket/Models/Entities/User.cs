6b519f3de98e5a90569270809d47a4ef554e3a71
ayub <ayub.arbiev@gmail.com>
2024-11-09 19:24:04 +0300

    add User entity

---
+++ DiscontMarket/Models/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using DiscontMarket.Domain.Models.Abstractions.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscontMarket.Domain.Models.Entities
+{
+    public class User : ApplicationUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public List<Product>? Products { get; set; }
+    }
+}
