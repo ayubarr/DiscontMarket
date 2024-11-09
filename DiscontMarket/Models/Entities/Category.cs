3cf50cbdba66070276a0cedf4dd5c37dbb670887
Vladislav <vlad.maslyukov.04@gmail.com>
2024-11-10 01:05:23 +0300

    Added Category

---
+++ DiscontMarket/Models/Entities/Category.cs
@@ -0,0 +1,18 @@
+using DiscontMarket.Domain.Models.Abstractions.BaseEntities;
+
+namespace DiscontMarket.Domain.Models.Entities
+{
+    
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        
+        public uint? UserID { get; set; }
+        public User? User { get; set; }
+        
+        public uint? ProductID { get; set; }
+        public Product? Product { get; set; }
+    }
+}
