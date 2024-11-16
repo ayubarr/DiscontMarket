9d4840ad47d56ab0d7a4c57229e6e7a479d449a6
ayub <ayub.arbiev@gmail.com>
2024-11-16 22:12:10 +0300

    add UserConfiguration

---
+++ DiscontMarket.DAL/SqlServer/Configuration/UserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using DiscontMarket.DAL.SqlServer.Helpers;
+using DiscontMarket.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiscontMarket.DAL.SqlServer.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            PropertyHelper<User>.SetProperties(builder, false,
+               e => e.UserName,
+               e => e.NormalizedUserName,
+               e => e.FirstName,
+               e => e.LastName,
+               e => e.MiddleName,
+               e => e.Email,
+               e => e.NormalizedEmail,
+               e => e.PasswordHash,
+               e => e.SecurityStamp,
+               e => e.ConcurrencyStamp);
+        }
+    }    
+}
