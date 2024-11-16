b5ba1ea15d7f00e18eab868926fe957beb05b982
ayub <ayub.arbiev@gmail.com>
2024-11-16 22:09:56 +0300

    fix

--- DiscontMarket/Models/Enums/Condition.cs
+++ DiscontMarket/Models/Enums/Condition.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiscontMarket.Domain.Models.Enums
+﻿namespace DiscontMarket.Domain.Models.Enums
 {
     public enum Condition
     {
