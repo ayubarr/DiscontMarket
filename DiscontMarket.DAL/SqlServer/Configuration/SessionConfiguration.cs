838ae2d860871bd92ed25e2d643cd7dc0c1ca2c2
ayub <ayub.arbiev@gmail.com>
2024-11-16 22:12:18 +0300

    add SessionConfiguration

---
+++ DiscontMarket.DAL/SqlServer/Configuration/SessionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using DiscontMarket.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiscontMarket.DAL.SqlServer.Configuration
+{
+    public class SessionConfiguration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.HasKey(s => s.ID);
+        }
+    }
+}
