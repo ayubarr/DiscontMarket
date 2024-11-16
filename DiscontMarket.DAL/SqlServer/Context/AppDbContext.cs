cdc869e44c3f1104cf4c50ed1502bc4aab636563
ayub <ayub.arbiev@gmail.com>
2024-11-16 22:10:25 +0300

    add DbContext class with PgSql and Identity

---
+++ DiscontMarket.DAL/SqlServer/Context/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using DiscontMarket.Domain.Models.Abstractions.BaseEntities;
+using DiscontMarket.Domain.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscontMarket.DAL.SqlServer.Context
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, uint>
+    {
+        public AppDbContext() : base()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Brend> Brends { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<AttributeEntity> AttributeEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+        }
+    }
+}
