9dafe695bd17cb867e785f7cd1b2929eccedde2e
ayub <ayub.arbiev@gmail.com>
2024-11-16 22:12:25 +0300

    add ProductConfiguration

---
+++ DiscontMarket.DAL/SqlServer/Configuration/ProductConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using DiscontMarket.DAL.SqlServer.Helpers;
+using DiscontMarket.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiscontMarket.DAL.SqlServer.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.ID);
+
+            PropertyHelper<Product>.SetProperties(builder, false,
+               p => p.ProductName
+            );
+
+            PropertyHelper<Product>.SetProperties(builder,
+                p => p.Cost
+            );
+
+            PropertyHelper<Product>.SetProperties(builder,
+                p => p.AvailStatus
+            );
+
+            builder.HasOne(p => p.User)
+                .WithMany(u => u.Products)
+                .HasForeignKey(p => p.UserID);
+            
+        }
+    }
+}
