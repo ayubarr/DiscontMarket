5fa9b3baed35b9056d543684258ebd050a421e37
ayub <ayub.arbiev@gmail.com>
2024-11-17 23:37:07 +0300

    add base repository

---
+++ DiscontMarket.DAL/Repository/Implementations/BaseRepository.cs
@@ -0,0 +1,83 @@
+﻿using DiscontMarket.DAL.Repository.Interfaces;
+using DiscontMarket.DAL.SqlServer.Context;
+using DiscontMarket.Domain.Models.Abstractions.BaseEntities;
+using DiscontMarket.Validation;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscontMarket.DAL.Repository.Implementations
+{
+    public class BaseRepository<T> : IBaseRepository<T>
+        where T : BaseEntity
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public BaseRepository(AppDbContext context)
+        {
+            ObjectValidator<AppDbContext>.CheckIsNotNullObject(context);
+
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task Create(T entity)
+        {
+             ObjectValidator<T>.CheckIsNotNullObject(entity);
+
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            ObjectValidator<T>.CheckIsNotNullObject(entity);
+
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            return await Task.FromResult(_dbSet);
+        }
+
+        public T GetById(uint id)
+        {
+            NumberValidator<uint>.IsNotZero(id);
+
+            var entity = GetAll().FirstOrDefault(x => x.ID == id);
+            ObjectValidator<T>.CheckIsNotNullObject(entity);
+            return entity;
+        }
+
+        public async Task<T> GetByIdAsync(uint id)
+        {
+            NumberValidator<uint>.IsNotZero(id);
+
+            var entity = await GetAllAsync().Result.FirstOrDefaultAsync(x => x.ID == id);
+            ObjectValidator<T>.CheckIsNotNullObject(entity);
+            return entity;
+        }
+
+        public async Task DeleteById(uint id)
+        {
+            var entity = await GetByIdAsync(id);
+            await Delete(entity);
+        }
+
+        public async Task Delete(T entity)
+        {
+            ObjectValidator<T>.CheckIsNotNullObject(entity);
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
---
+++ DiscontMarket.DAL/Repository/Interfaces/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using DiscontMarket.Domain.Models.Abstractions.BaseEntities;
+
+namespace DiscontMarket.DAL.Repository.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        public Task Create(T entity);
+
+        public IQueryable<T> GetAll();
+
+        public T GetById(uint id);
+
+        public Task<IQueryable<T>> GetAllAsync();
+
+        public Task<T> GetByIdAsync(uint id);
+
+        public Task Update(T entity);
+
+        public Task Delete(T entity);
+
+        public Task DeleteById(uint id);
+
+    }
+}
