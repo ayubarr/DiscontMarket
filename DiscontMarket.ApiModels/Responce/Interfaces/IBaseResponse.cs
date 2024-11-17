f756f88f19cd540f409271c8acb6eb6504e5408c
ayub <ayub.arbiev@gmail.com>
2024-11-18 01:42:42 +0300

    add response interface

---
+++ DiscontMarket.ApiModels/Responce/Interfaces/IBaseResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace DiscontMarket.ApiModels.Responce.Interfaces
+{
+    public interface IBaseResponse<T>
+    {
+        /// <summary>
+        /// Gets or sets the data associated with the response.
+        /// </summary>
+        T Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status code of the response.
+        /// </summary>
+        int StatusCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message describing the response.
+        /// </summary>
+        string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the response is a success or not.
+        /// </summary>
+        bool IsSuccess { get; set; }
+    }
+}
