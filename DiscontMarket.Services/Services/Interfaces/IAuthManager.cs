115ba050679912a52313a959038aaccc2c29bff8
ayub <ayub.arbiev@gmail.com>
2024-11-18 01:47:18 +0300

    add Auth interfaces

---
+++ DiscontMarket.Services/Services/Interfaces/IAuthManager.cs
@@ -0,0 +1,27 @@
+﻿using DiscontMarket.ApiModels.Auth;
+using DiscontMarket.ApiModels.Responce.Interfaces;
+using DiscontMarket.Domain.Models.Abstractions.BaseEntities;
+
+namespace DiscontMarket.Services.Services.Interfaces
+{
+    /// <summary>
+    /// Определяет методы для управления аутентификацией и авторизацией пользователей.
+    /// </summary>
+    /// <typeparam name="Tmodel">Тип модели пользователя, наследующей ApplicationUser.</typeparam>
+    public interface IAuthManager<Tmodel> where Tmodel : ApplicationUser
+    {
+        /// <summary>
+        /// Выполняет процесс входа пользователя в систему.
+        /// </summary>
+        /// <param name="model">Модель, содержащая учетные данные для входа.</param>
+        /// <returns>Объект, представляющий результат операции аутентификации.</returns>
+        Task<IBaseResponse<AuthResultStruct>> Login(LoginModel model);
+
+        /// <summary>
+        /// Выполняет процесс регистрации нового пользователя.
+        /// </summary>
+        /// <param name="model">Модель, содержащая данные нового пользователя для регистрации.</param>
+        /// <returns>Объект, представляющий результат операции регистрации.</returns>
+        Task<IBaseResponse<uint>> Register(RegisterModel model);
+    }
+}
---
+++ DiscontMarket.Services/Services/Interfaces/ITokenManager.cs
@@ -0,0 +1,33 @@
+﻿using DiscontMarket.ApiModels.Auth;
+using DiscontMarket.ApiModels.Responce.Interfaces;
+using DiscontMarket.Domain.Models.Abstractions.BaseEntities;
+
+namespace DiscontMarket.Services.Services.Interfaces
+{
+    /// <summary>
+	/// Менеджер для работы с токенами
+	/// </summary>
+	public interface ITokenManager<TModel>
+        where TModel : ApplicationUser
+    {
+        /// <summary>
+        /// Обновляет токен доступа, используя предоставленную модель токена
+        /// </summary>
+        /// <param name="model"> Модель токена</param>
+        /// <returns> Результат обновления токена </returns>
+        public Task<IBaseResponse<AuthResultStruct>> UpdateToken(TokenModel model);
+
+        /// <summary>
+        /// Отзывает токен обновления для пользователя с указанным именем пользователя.
+        /// </summary>
+        /// <param name="username">Имя пользователя, для которого нужно отозвать токен обновления.</param>
+        /// <returns>Задача, представляющая асинхронную операцию и содержащая <see cref="IBaseResponse{T}"/>, где T - логическое значение, указывающее на успешность или неудачу отзыва токена.</returns>
+        public Task<IBaseResponse<bool>> RevokeRefreshTokenByUsernameAsync(string username);
+
+        /// <summary>
+        /// Отзывает все токены обновления.
+        /// </summary>
+        /// <returns>Задача, представляющая асинхронную операцию и содержащая <see cref="IBaseResponse{T}"/>, где T - логическое значение, указывающее на успешность или неудачу отзыва всех токенов обновления.</returns>
+        public Task<IBaseResponse<bool>> RevokeAllRefreshTokensAsync();
+    }
+}
