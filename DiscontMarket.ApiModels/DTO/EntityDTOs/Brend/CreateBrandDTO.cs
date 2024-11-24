79c5fefdadf2d1f577ff8ff3defe2fafb2f9ab2d
Vlad Maslyukov <vlad.maslyukov.04@mail.ru>
2024-11-25 01:28:07 +0300

    Added all DTOEntinies for Brand, Category, TagDTO.

---
+++ DiscontMarket.ApiModels/DTO/EntityDTOs/Brend/CreateBrandDTO.cs
@@ -0,0 +1,11 @@
+﻿using DiscontMarket.ApiModels.DTO.BaseDTOs;
+
+namespace DiscontMarket.ApiModels.DTO.EntityDTOs.Brand
+{
+    public class CreateBtandDTO : BaseDTO
+    {
+        public string BrandName { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
