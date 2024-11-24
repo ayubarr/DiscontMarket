61ce0fde4edabcc5ce403d0e1df15cfb1f42ff4a
Vlad Maslyukov <vlad.maslyukov.04@mail.ru>
2024-11-24 21:07:35 +0300

    Added entity Image. Create configuration Image.

--- DiscontMarket.DAL/SqlServer/Configuration/CategoryConfiguration.cs
+++ DiscontMarket.DAL/SqlServer/Configuration/CategoryConfiguration.cs
@@ -18,11 +18,6 @@ namespace DiscontMarket.DAL.SqlServer.Configuration
                 c => c.Type
             );
 
-            //Описание связи сущностей Category и User
-            builder.HasOne(c => c.User)
-                .WithMany(u => u.Categories)
-                .HasForeignKey(c => c.UserID);
-
             //Описание связи сущностей Category и Product
             builder.HasOne(c => c.Product)
                .WithMany(p => p.Categories)
