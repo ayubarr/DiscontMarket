81fede88150fe06b3c4c4e7d116e563ced151eb7
ayub <ayub.arbiev@gmail.com>
2024-11-25 19:28:18 +0300

    add Create Product

--- DiscontMarket.Services/Services/Implementations/ProductService.cs
+++ DiscontMarket.Services/Services/Implementations/ProductService.cs
@@ -2,6 +2,7 @@
 using DiscontMarket.ApiModels.Responce.Helpers;
 using DiscontMarket.ApiModels.Responce.Interfaces;
 using DiscontMarket.DAL.Repository.Interfaces;
+using DiscontMarket.Domain.Models.Abstractions.LinkEntities;
 using DiscontMarket.Domain.Models.Entities;
 using DiscontMarket.Domain.Models.Enums;
 using DiscontMarket.Services.Helpers.Filter;
@@ -13,10 +14,100 @@ namespace DiscontMarket.Services.Services.Implementations
     public class ProductService : BaseService<Product>, IProductService
     {
         private readonly IProductRepository _productRepository;
+        private readonly IBaseRepository<Brand> _brandRepository;
+        private readonly IBaseRepository<Category> _categoryRepository;
+        private readonly IBaseRepository<AttributeEntity> _attributeRepository;
 
-        public ProductService(IProductRepository repository) : base(repository)
+        public ProductService(IProductRepository productRepository, IBaseRepository<Brand> brandRepository,
+            IBaseRepository<Category> categoryRepository, IBaseRepository<AttributeEntity> attributeRepository) : base(productRepository)
         {
-            _productRepository = repository;
+            _productRepository = productRepository;
+            _brandRepository = brandRepository;
+            _categoryRepository = categoryRepository;
+            _attributeRepository = attributeRepository;
+        }
+
+        public IBaseResponse<Product> CreateProduct(CreateProductDTO entityDTO)
+        {
+            try
+            {
+                ObjectValidator<CreateProductDTO>.CheckIsNotNullObject(entityDTO);
+
+                var brand = _brandRepository
+                    .GetAll()
+                    .Where(x => x.Name.Equals(entityDTO.BrandName))
+                    .FirstOrDefault();
+
+                ObjectValidator<Brand>.CheckIsNotNullObject(brand);
+
+                var categories = _categoryRepository.GetAll()
+                    .Where(x => x.Name.Equals(entityDTO.CategoryName)).ToList();
+
+                ObjectValidator<List<Category>>.CheckIsNotNullObject(categories);
+
+                var attributes = new List<AttributeEntity>();
+                foreach (var attributeName in entityDTO.AttributeNames)
+                {
+                    if (_attributeRepository.GetAll().Any(a => a.Name.Equals(attributeName)))
+                        attributes
+                            .Add(_attributeRepository
+                            .GetAll()
+                            .Where(a => a.Name.Equals(attributeName))
+                            .First());
+                }
+
+                if (attributes.Count == 0)
+                {
+                    throw new Exception($"not found atributes");
+                }
+
+
+                Availability availability;
+                if (!Enum.TryParse(entityDTO.Availability, out availability))
+                {
+                    throw new Exception($"not found availability {entityDTO.Availability}");
+                }
+
+                availability = (Availability)Enum.Parse(typeof(Availability), entityDTO.Availability);
+
+
+                ProductStatus productStatus;
+                if (!Enum.TryParse(entityDTO.Status, out productStatus))
+                {
+                    throw new Exception($"not found product status {entityDTO.Status}");
+                }
+
+                productStatus = (ProductStatus)Enum.Parse(typeof(Availability), entityDTO.Availability);
+
+
+                var entity = new Product
+                {
+                    ProductName = entityDTO.ProductName,
+                    Price = entityDTO.Price,
+                    Quantity = entityDTO.Quantity,
+                    ImagePath = entityDTO.ImagePath,
+                    Availability = availability,
+                    Status = productStatus,
+                    Brand = brand,
+                    Categories = categories,
+                    ProductAttributes = attributes.Select(attribute => new ProductAttribute
+                    {
+                        Attribute = attribute
+                    }).ToList()
+                };
+
+                _productRepository.Create(entity);
+
+                return ResponseFactory<Product>.CreateSuccessResponse(entity);
+            }
+            catch (ArgumentNullException argNullException)
+            {
+                return ResponseFactory<Product>.CreateNotFoundResponse(argNullException);
+            }
+            catch (Exception exception)
+            {
+                return ResponseFactory<Product>.CreateErrorResponse(exception);
+            }
         }
 
         public IBaseResponse<IEnumerable<Product>> GetAllProducts(FilterProductDTO filterProductDTO, SortTypes? sortType)
--- DiscontMarket.Services/Services/Interfaces/IProductService.cs
+++ DiscontMarket.Services/Services/Interfaces/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using DiscontMarket.ApiModels.DTO.EntityDTOs.Product;
+﻿using DiscontMarket.ApiModels.DTO.BaseDTOs;
+using DiscontMarket.ApiModels.DTO.EntityDTOs.Product;
 using DiscontMarket.ApiModels.Responce.Interfaces;
 using DiscontMarket.Domain.Models.Entities;
 using DiscontMarket.Domain.Models.Enums;
@@ -8,5 +9,7 @@ namespace DiscontMarket.Services.Services.Interfaces
     public interface IProductService : IBaseService<Product>
     {
         IBaseResponse<IEnumerable<Product>> GetAllProducts(FilterProductDTO filterProductDTO, SortTypes? sortProductDTO);
+        IBaseResponse<Product> CreateProduct(CreateProductDTO entityDTO);
+
     }
 }
