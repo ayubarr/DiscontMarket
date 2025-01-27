097c7f1742337f82ea99dfa3564a8049e53b47d4
OxYxO <ayub.arbiev@gmail.com>
2025-01-27 16:38:30 +0600

    changes

--- DiscontMarket/Models/Entities/User.cs
+++ DiscontMarket/Models/Entities/User.cs
@@ -10,10 +10,30 @@ namespace DiscontMarket.Domain.Models.Entities
 
         public string MiddleName { get; set; }
 
+        public string? SupportContacts { get; set; }
+        
         public string? ClientsVk { get; set; }
+        
         public string? ClientsTelegram { get; set; }
+        
         public string? ClientsWhatsapp { get; set; }
+        
+        public string? WorkTimeInfo { get; set; }
+        
+        public string? PhoneNumber { get; set; }
+        
+        public string? ReturnsText  { get; set; }
+        
+        public string? TextAdress { get; set; }
+        
+        public string? HrefAdress { get; set; }
+        
+        public string? HrefmapAdress { get; set; }
 
+        public string? ContactInfoText { get; set; }
+        
+        public string? Email { get; set; }
+        
         public List<Product>? Products { get; set; }
     }
 }
